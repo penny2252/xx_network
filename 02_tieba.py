c442fbbf80b50c077a0d2e0898a1953ee24685e3
penny2252 <48152033+penny2252@users.noreply.github.com>
2019-03-31 18:54:39 +0800

    Update 02_tieba.py

--- 02_tieba.py
+++ 02_tieba.py
@@ -1,10 +1,11 @@
+
 import os
 import request
 
 
 
 def web(name,num):
-    url='http://www.baidu.com/s?wk={}&nq={}'.format(name,(num-1)*50)
+    url='http://tieba.baidu.com/f?kw={}&nq={}'.format(name,(num-1)*50)
     headers={'User_Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
     q=request.get(url,headers=headers)
     txt=q.content.decode()
@@ -24,7 +25,7 @@ def create_file(forder,num):
         
 def main():
     #创建文件写入爬取的内容
-    tieba_name='liyiba'
+    tieba_name='李毅吧’
     num=100
     create_file(tieba_name,100)
 
